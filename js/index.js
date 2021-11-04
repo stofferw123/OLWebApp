8c4ca6ff1ede781d9a3b1797dc7aeb2a2c746d66
stofferw123 <christofferwith121001@gmail.com>
2021-11-04 12:17:51 +0100

    first commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+const baseUrl = "https://athleterest20211104093650.azurewebsites.net/api/athletes"
+
+Vue.createApp({
+    data() {
+        return {
+            Athletes: [],
+            addData: { name: "", country: "", height: 0},
+            addMessage: "",
+            deleteMessage: "",
+        }
+    },
+
+    methods: {
+        getAllAthletes() {
+            this.helperGetAndShow(baseUrl)
+        },
+        async helperGetAndShow(url) {
+            try {
+                const response = await axios.get(url)
+                this.Athletes = await response.data
+            } catch(ex) {
+                alert(ex.message)
+            }
+        },
+        async addAthlete() {
+            try {
+                response = await axios.post(baseUrl, this.addData)
+                this.addMessage = "response" + response.status + " " + response.statusText
+                this.getAllAthletes()
+            } catch(ex) {
+                alert(ex.message)
+            }
+        },
+        async deleteAthlete(deleteId) {
+            const url = baseUrl + "/" + deleteId
+            try {
+                response = await axios.delete(url)
+                this.deleteMessage = response.status + " " + response.statusText
+                this.getAllAthletes()
+            } catch(ex) {
+                alert(ex.message)
+            }
+        }
+    }
+
+}).mount("#app")
